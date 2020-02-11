ca605c006a11df76bd265ef97299727a2d3eb73b
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-11 06:44:07 +0000

    now

--- action_razorpay.php
+++ action_razorpay.php
@@ -15,40 +15,23 @@ if(isset($_POST['submit'])){
 
     if($email_ok && $phone_ok && $agree_ok){
 
-        ?>
-        <button id="rzp-button1">Pay</button>
-        <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
-        <script>
-        var options = {
-            "key": "rzp_test_0iFYQGkeOpElYh", // Enter the Key ID generated from the Dashboard
-            "amount": "142100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise or INR 500.
-            "currency": "INR",
-//            "name": "Acme Corp",
-//            "description": "A Wild Sheep Chase is the third novel by Japanese author  Haruki Murakami",
-//            "image": "https://example.com/your_logo",
-//            "order_id": "order_9A33XWu170gUtm",//This is a sample Order ID. Create an Order using Orders API. (https://razorpay.com/docs/payment-gateway/orders/integration/#step-1-create-an-order). Refer the Checkout form table given below
-            "handler": function (response){
-                alert(response.razorpay_payment_id);
-            },
-            "prefill": {
-                "name": $fname,
-                "email": $email,
-                "contact": $phone
-            },
-            "notes": {
-                "child's name": "child's name",
-                "child's grade": "child's grade"
-            },
-            "theme": {
-                "color": "#F37254"
-            }
-        };
-        var rzp1 = new Razorpay(options);
-        document.getElementById('rzp-button1').onclick = function(e){
-            rzp1.open();
-            e.preventDefault();
-        }
-        </script>
+?>
+        <form action="https://www.codingkidsnow.com/thank-you" method="POST">
+        <script
+            src="https://checkout.razorpay.com/v1/checkout.js"
+            data-key="rzp_test_0iFYQGkeOpElYh" // Enter the Key ID generated from the Dashboard
+            data-amount="142100" // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise or INR 500.
+            data-currency="INR"
+            data-buttontext="Pay with Razorpay"
+            data-prefill.name=$fname
+            data-prefill.email=$email
+            data-prefill.contact=$phone
+            data-notes.childsname="Child's name"
+            data-notes.childsgrade="Child's grade"
+        ></script>
+        <input type="hidden" custom="Hidden Element" name="hidden">
+        </form>
+
 <?php        
 
     }else{
