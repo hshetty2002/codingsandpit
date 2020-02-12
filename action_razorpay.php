3d5d02b6e42b05b600dbea9a57d68fa9fa297375
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-12 06:13:29 +0000

    now

--- action_razorpay.php
+++ action_razorpay.php
@@ -12,21 +12,18 @@ if(isset($_POST['submit'])){
     if($standard_ok && $agree_ok){
 
 ?>
-<!--        <form action="https://www.codingkidsnow.com/thank-you" method="POST">
--->        <script
+        <form action="https://www.codingkidsnow.com/thank-you" method="POST">
+        <script
             src="https://checkout.razorpay.com/v1/checkout.js"
-            data-key="rzp_test_0iFYQGkeOpElYh" // Enter the Key ID generated from the Dashboard
+            data-key="rzp_test_0iFYQGkeOpElYh" //this key is for the Test mode
             data-amount="142100" // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise or INR 500.
             data-currency="INR"
             data-buttontext="Pay with Razorpay"
-            data-prefill.name=<?php echo $fname; ?>
-            data-prefill.email=<?php echo $email; ?>
-            data-prefill.contact=<?php echo $phone; ?>
-            data-notes.childsname="Child's name"
-            data-notes.childsgrade="Child's grade"
+            data-notes.childname=<?php echo $fname; ?>
+            data-notes.childstandard=<?php echo $standard; ?>
         ></script>
-<!--        <input type="hidden" custom="Hidden Element" name="hidden">
--->        </form>
+        <input type="hidden" custom="Hidden Element" name="hidden">
+        </form>
 
 <?php
 
