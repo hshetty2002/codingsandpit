5f7e8d7eb3f9384523fc1caaa5a13fd4c9c50513
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-12 10:22:32 +0000

    now

--- razorpay/rpRequestHandler.php
+++ razorpay/rpRequestHandler.php
@@ -26,10 +26,10 @@
 
 <!--
 	<form method="post" name="redirect" target='_parent' action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
-	<?php
-		echo "<input type=hidden name=encRequest value=$encrypted_data>";
-		echo "<input type=hidden name=access_code value=$access_code>";
-	?>
+//	<?php
+//		echo "<input type=hidden name=encRequest value=$encrypted_data>";
+//		echo "<input type=hidden name=access_code value=$access_code>";
+//	?>
 	</form>
 	<script language='javascript'>document.redirect.submit();</script>
 
