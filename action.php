05adfa2c6b4829de2523d7f5bc07984b24a3dc75
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-12 16:08:31 +0000

    now

--- action.php
+++ action.php
@@ -1,15 +1,27 @@
 <?php session_start(); // place it on the top of the script ?>
-<?php 
-     $fname = $_SESSION['fname'];
-    $standard = $_SESSION['standard'];
+<?php
+if(isset($_POST['submit'])){
+    $fname = $_POST['fname'];
+    $standard = $_POST['standard'];
+
+    $agree = $_POST['agree'];
+    
+    if(!empty($standard) && $standard != 0){ $standard_ok = 1;} else { $standard_ok = 0;}
+    if(!empty($agree) && ($agree === "YES")) { $agree_ok = 1;} else { $agree_ok = 0;}
+
+    if($standard_ok && $agree_ok){
+
+        $_SESSION['fname'] = $fname;
+        $_SESSION['standard'] = $standard;
+        header('location:razorpay/rpRequestHandler.php');
+
+    }else{
+        $_SESSION['msg'] = '<p style="color: #EA4335">Please enter valid standard and agree to Terms of Use. </p>';
+        $_SESSION['fname'] = $fname;
+        $_SESSION['standard'] = $standard;
+
+        header('location:index.php');
+    }
+}
+
 ?>
-
-
-<script
-    src="https://cdn.razorpay.com/static/checkout/wix.js"
-    data-key="rzp_test_0iFYQGkeOpElYh"
-    data-amount="142100" 
-    data-currency="INR"
-    data-notes.childname="Harsh"
-    data-notes.childstandard="2"
-></script>
--- razorpay/rpRequestHandler.php
+++ razorpay/rpRequestHandler.php
@@ -8,11 +8,10 @@
 <body>
 <center>
  
-<!--   If coming back from ccavenue payment page, then go back to mailchimp index page (otherwise this will resubmit) -->
+<!--   If coming back from payment page, then go back to mailchimp index page (otherwise this will resubmit) -->
  
  <?php
  if(performance.navigation.type == 2){
-//   	location.reload(true);
    header('location:../index.php');
  }
  ?>
@@ -25,51 +24,18 @@
 
 ?>
 
-<!--
-	<form method="post" name="redirect" target='_parent' action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
-//	<?php
-//		echo "<input type=hidden name=encRequest value=$encrypted_data>";
-//		echo "<input type=hidden name=access_code value=$access_code>";
-//	?>
-	</form>
-	<script language='javascript'>document.redirect.submit();</script>
-
--->
-
     <form action="https://codingkidsnow.herokuapp.com/thankyou.php" method="POST">
     <script
         src="https://checkout.razorpay.com/v1/checkout.js"
         data-key="rzp_test_0iFYQGkeOpElYh" //this key is for the Test mode
         data-amount="142100" // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise or INR 500.
         data-currency="INR"
-        data-notes.childname=<?php echo $fname; ?>
-        data-notes.childstandard=<?php echo $standard; ?>
-    ></script>
-    </form>
-	<script language='javascript'>document.redirect.submit();</script>
-
-<!-- Old
-
-    <form action="https://codingkidsnow.herokuapp.com/thankyou.php" method="POST">
-    <script
-        src="https://checkout.razorpay.com/v1/checkout.js"
-        data-key="rzp_test_0iFYQGkeOpElYh" //this key is for the Test mode
-        data-amount="142100" // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise or INR 500.
-        data-currency="INR"
-        data-buttontext="Pay with Razorpay"
+        data-buttontext="Pay"
         data-notes.childname=<?php echo $fname; ?>
         data-notes.childstandard=<?php echo $standard; ?>
     ></script>
     <input type="hidden" custom="Hidden Element" name="hidden">
     </form>
-
-
--->
-
-
-
-
-</center>
 
 </body>
 </html>
