9cad63117871f3b7911d8db0e35cebe325d142d8
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-13 01:26:12 +0000

    now

--- razorpay/rpRequestHandler.php
+++ razorpay/rpRequestHandler.php
@@ -3,17 +3,18 @@
 <html>
 <head>
 <title>Razorpay Payment</title>
-.button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-}
-<script type="text/javascript" src="https://checkout.razorpay.com/v1/razorpay.js"></script>
+    <style type="text/css">
+    .button {
+      background-color: #4CAF50; /* Green */
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+    }
+    </style>
 </head>
 <body>
 <center>
