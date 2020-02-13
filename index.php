9263ad41cf7bee496868d04b26b094ec076d3092
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-13 02:11:12 +0000

    now

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
     $statusMsg = !empty($_SESSION['msg'])?$_SESSION['msg']:'';
     $prevFName = !empty($_SESSION['fname'])?$_SESSION['fname']:'';
     $prevStandard = !empty($_SESSION['standard'])?$_SESSION['standard']:'';
+    $prevPFName = !empty($_SESSION['pfname'])?$_SESSION['pfname']:'';
+    $prevPhone = !empty($_SESSION['phone'])?$_SESSION['phone']:'';
+    $prevEmail = !empty($_SESSION['email'])?$_SESSION['email']:'';
 
     unset($_SESSION['msg']);
     unset($_SESSION['fname']);
     unset($_SESSION['standard']);
+    unset($_SESSION['pfname']);
+    unset($_SESSION['phone']);
+    unset($_SESSION['email']);
     echo $statusMsg;
 
 ?>
@@ -54,6 +60,15 @@
             <option value="2">Standard 7</option>
             <option value="2">Standard 8</option>
         </select>
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" name="pfname" placeholder="Parent's name" value=<?php echo $prevPFName; ?> >
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" name="phone" placeholder="Parent's phone" value=<?php echo $prevPhone; ?> >
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" name="email" placeholder="Parent's email" value=<?php echo $prevEmail; ?> >
     </div>
     <p><input type="checkbox" name="agree" value="YES"> I, the parent of the child listed above, accept the Terms of Use, Privacy Policy and Refund Policy.</input></p>
 	<p><button type="submit" class="btn btn-default" name="submit" value="REGISTER">Register</button></p>
