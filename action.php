7c328f52c9eae9fa7e542d9e751cb08d6c97985b
Harsh Shetty <harsh.shetty@gmail.com>
2020-02-13 03:50:53 +0000

    now

--- action.php
+++ action.php
@@ -24,6 +24,42 @@ if(isset($_POST['submit'])){
         $_SESSION['email'] = $email;
         $_SESSION['address'] = $address;
 
+        // put it into MailChimp
+        $apiKey = '5b261f7e714f9bc9d0d426dbb8919a67-us19';
+        $listID = 'f90c6f4526';
+
+        // MailChimp API URL
+        $memberID = md5(strtolower($email));
+        $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . $memberID;
+
+        // member information
+        $json = json_encode([
+            'email_address' => $email,
+            'status'        => 'subscribed',
+            'merge_fields'  => [
+                'FNAME'     => $fname,
+                'STANDARD'  => $standard,
+                'PFNAME'    => $pfname,
+                'PHONE'     => $phone,
+                'ADDRESS'   => $address,
+                'AGREE'     => $agree
+            ]
+        ]);
+        
+        // send a HTTP POST request with curl
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
         header('location:razorpay/rpRequestHandler.php');
 
     }else{
